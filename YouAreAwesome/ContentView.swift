6e777c65f87eaf5f50fb9b5d46fbdff6095eca5a
Derdrom-TJM <94881069+Derdrom-TJM@users.noreply.github.com>
2025-01-27 21:47:43 -0600

    Lesson 3.  Two Buttons, .buttonStyle, Spacer(), .frame modifier, selecting optional parameters from the autocomplete!

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,17 +11,37 @@ struct ContentView: View {
     @State private var message = "I am a programmer!"
     var body: some View {
         VStack {
+            Spacer()
+            
             Image(systemName:"swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+                
+            
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click me!") {
-                message = "Awesome!"
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
+            
+            Spacer()
+            
+            HStack {
+                Spacer()
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                Spacer()
+                Button("Great") {
+                    message = "Great!"
+                }
+                Spacer()
             }
+            
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
