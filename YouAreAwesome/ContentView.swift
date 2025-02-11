04952958e734a18a9d6f388fbaacb1b0a206aca9
Derdrom-TJM <94881069+Derdrom-TJM@users.noreply.github.com>
2025-02-10 21:34:11 -0600

    Differences between constants and variables, used conditionals, learned about scope and what pertains to scope

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var counter = 0
-
+    
     var body: some View {
         VStack {
             Spacer()
@@ -20,8 +20,8 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-                //.frame(width: 200, height: 200)
-                
+            //.frame(width: 200, height: 200)
+            
             
             Text(message)
                 .font(.largeTitle)
@@ -29,22 +29,22 @@ struct ContentView: View {
                 .foregroundStyle(.black)
             
             Spacer()
-            
-            HStack {
-                Button("Press Me") {
-                    
-                    if counter % 2 == 0{
-                        message = "You are Awesome!"
-                        imageName = "sun.max.fill"
-                    } else {
-                        message = "You are Great!"
-                        imageName = "hand.thumbsup"
-                    }
-                    counter += 1
-                    }
-
+            // CMD + A to highlight and Ctrl + i to fix indent
+            Button("Press Me") {
+                //Could have used else to change it from "" to you are awesome
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+                let imageString1 = "sun.max.fill"
+                let imageString2 = "hand.thumbsup"
+                if counter % 2 == 0{
+                    message = message1
+                    imageName = imageString1
+                } else {
+                    message = message2
+                    imageName = imageString2
+                } //Talk about scope as since they don't exist outside the button
+                counter += 1
             }
-            
             .buttonStyle(.borderedProminent)
             .font(.title2)
             .tint(.orange)
