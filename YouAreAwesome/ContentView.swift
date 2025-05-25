8067987d3e216bf96fbbb7babcc756f96916ba99
Derdrom-TJM <94881069+Derdrom-TJM@users.noreply.github.com>
2025-05-25 13:07:09 -0500

    1.12 While Loop, Repeat While Loop, Infinite Loops, Explicity declare a variable without initializing, Errors for using a variable that is not initialized

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -16,7 +16,9 @@ struct ContentView: View {
                                    "Fabulous? That's You",
                                    "You Are A Swifty Now",
                                    "Keep It Rocking!", "You will be the Greatest App Developer in History"]
-    @State private var messageCounter = 0
+//    @State private var messageCounter = 0
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
     
     
     var body: some View {
@@ -38,54 +40,41 @@ struct ContentView: View {
                 .shadow(radius: 30)
                 .animation(.default, value: imageName)
 
-            //.frame(width: 200, height: 200)
             
             
 
             
             Spacer()
-            // CMD + A to highlight and Ctrl + i to fix indent
             Button("Show Message") {
-                //Could have used else to change it from "" to you are awesome
-//------1.9 Code Challenge-----------
-                
-                message = messages[Int.random(in: 0..<messages.count)]
-
-                
-//-----------------
-//                let message1 = "You are Awesome!"
-//                let message2 = "You are Great!"
-//                let message3 = "Fabulous? That's You"
+//While Loop
+//                var messageNumber = Int.random(in: 0..<messages.count)
 //
-//                if message == message1{
-//                    message = message2
-//                }else if message == message2{
-//                    message = message3
-//                }else {
-//                    message = message1
+//                while messageNumber == lastMessageNumber {
+//                    
+//                    messageNumber = Int.random(in: 0..<messages.count)
+//                    
 //                }
                 
                 
-                //let imageString1 = "image0"
-//                let imageString2 = "image1"
-                //removed string literals here and included below
-//                if counter % 2 == 0{
-//                    message = message1
-//                    imageName = imageString1
-//                } else {
-//                    message = message2
-//                    imageName = imageString2
-//                }
-                
-                //Talk about scope as since they don't exist outside the button
-                
-               //Removing this back to a standard if statement to follow video
-                //message = (counter % 2 == 0 ? message1 : message2 )
-//                imageName = (counter % 2 == 0 ? "image0" : "image1")
-                //my solution before the video.  I was already using a counter anyway
-                //TODO: Update the imageName variable
+                var messageNumber:Int
 
-                imageName = "image\(Int.random(in: 0...9))"
+                repeat {
+
+                    messageNumber = Int.random(in: 0..<messages.count)
+
+                }while messageNumber == lastMessageNumber
+                
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
+
+                
+
+                var imageNumber:Int
+                repeat {
+                    imageNumber = Int.random(in: 0...9)
+                } while imageNumber == lastImageNumber
+                lastImageNumber = imageNumber
+                imageName = ("image\(imageNumber)")
             }
             
             
@@ -100,14 +89,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-//Notes for 1.8
-//Data types are set by what type of information we store when they are initialized
-//Can't change type, option click to see type
-//Int - non decimal numbers
-//Strint - Text
-//Compound operator +=
-//show console on Xcode in lower right area debug
-//Left variables area, right console
-//CMD+K to clear the console
-//Breadcrumb code with TODO (top bar/jump bar)
-//TODO, FIXME, MARK, use dashes to make it more promeneent
