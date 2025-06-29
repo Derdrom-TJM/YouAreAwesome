21b44569176b8da86dde9fccf5be51eb87d2111d
Derdrom-TJM <94881069+Derdrom-TJM@users.noreply.github.com>
2025-06-29 11:05:37 -0500

    Refactored the random number generator for our selection of displays so they were function calls instead.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                                    "Fabulous? That's You",
                                    "You Are A Swifty Now",
                                    "Keep It Rocking!", "You will be the Greatest App Developer in History"]
-//    @State private var messageCounter = 0
+    //    @State private var messageCounter = 0
     @State private var lastMessageNumber = -1
     @State private var lastImageNumber = -1
     @State private var lastSoundNumber = -1
@@ -46,73 +46,84 @@ struct ContentView: View {
                 .clipShape(.rect(cornerRadius: 30))
                 .shadow(radius: 30)
                 .animation(.default, value: imageName)
-
             
             
-
+            
+            
             
             Spacer()
             Button("Show Message") {
-//While Loop
-//                var messageNumber = Int.random(in: 0..<messages.count)
-//
-//                while messageNumber == lastMessageNumber {
+                //While Loop
+                //                var messageNumber = Int.random(in: 0..<messages.count)
+                //
+                //                while messageNumber == lastMessageNumber {
+                //
+                //                    messageNumber = Int.random(in: 0..<messages.count)
+                //
+                //                }
+                
+                
+//                var messageNumber:Int
+//                
+//                repeat {
 //                    
 //                    messageNumber = Int.random(in: 0..<messages.count)
 //                    
-//                }
+//                }while messageNumber == lastMessageNumber
                 
                 
-                var messageNumber:Int
-
-                repeat {
-
-                    messageNumber = Int.random(in: 0..<messages.count)
-
-                }while messageNumber == lastMessageNumber
-                
-                message = messages[messageNumber]
-                lastMessageNumber = messageNumber
+                lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBounds: messages.count)
+                message = messages[lastMessageNumber]
 
                 
-
-                var imageNumber:Int
-                repeat {
-                    imageNumber = Int.random(in: 0...numberOfImages - 1)
-                } while imageNumber == lastImageNumber
-                lastImageNumber = imageNumber
-                imageName = ("image\(imageNumber)")
                 
-                var soundNumber:Int
-                repeat {
-                    soundNumber = Int.random(in: 0...numberOfSounds - 1)
-                }while soundNumber == lastSoundNumber
-                lastSoundNumber = soundNumber
-                soundName = "sound\(soundNumber)"
-
+//                var imageNumber:Int
+//                repeat {
+//                    imageNumber = Int.random(in: 0...numberOfImages - 1)
+//                } while imageNumber == lastImageNumber
+                lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBounds: numberOfImages)
+                imageName = ("image\(lastImageNumber)")
                 
-                guard let soundFile = NSDataAsset(name: soundName) else {
-                    print("😡 Could not read file named \(soundName)")
-                    return
-                }
-                do{
-                    try audioPlayer = AVAudioPlayer(data: soundFile.data)
-                    audioPlayer.play()
-
-                } catch{
-                    print("😡 ERROR: \(error.localizedDescription) creating audioPlayyer")
-                }
+//                var soundNumber:Int
+//                repeat {
+//                    soundNumber = Int.random(in: 0...numberOfSounds - 1)
+//                }while soundNumber == lastSoundNumber
+                lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBounds: numberOfSounds)
+                soundName = "sound\(lastSoundNumber)"
                 
+                playSound()
             }
-            
-            
             .buttonStyle(.borderedProminent)
             .font(.title2)
             //.tint(.orange)
         }
         .padding()
     }
+    
+    func playSound(){
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        do{
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+            
+        } catch{
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayyer")
+        }
+        
+    }
+    
+    func nonRepeatingRandom(lastNumber: Int, upperBounds: Int) -> Int{
+        var newNumber:Int
+        repeat {
+            newNumber = Int.random(in: 0...upperBounds - 1)
+        } while newNumber == lastImageNumber
+        return newNumber
+    }
 }
+
 //Great Loops program for 1.14 in forLoops Playground
 
 #Preview {
